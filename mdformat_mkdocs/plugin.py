ba1070dc9ec02931d38c53f90b196efd1ccc2da8
Kyle King <KyleKing@users.noreply.github.com>
2022-12-20 21:14:38 -0500

    feat: make functional

--- mdformat_mkdocs/plugin.py
+++ mdformat_mkdocs/plugin.py
@@ -1,25 +1,41 @@
 import textwrap
 from typing import Mapping
 from functools import partial
+
 from markdown_it import MarkdownIt
 from mdformat.renderer import RenderContext, RenderTreeNode
 from mdformat.renderer.typing import Render
 
 
 def update_mdit(mdit: MarkdownIt) -> None:
+    """No changes to markdown parsing are necessary."""
     ...
 
 
-def _render_list(node: RenderTreeNode, context: RenderContext, message: str) -> str:
+def _render_list(node: RenderTreeNode, context: RenderContext, bullet: str) -> str:
     """Render a `RenderTreeNode` consistent with `mkdocs`."""
-    return ""
+    indent = " " * 4  # MkDocs requires four spaces
+    rendered = ""
+    with context.indented(len(indent)):  # Modifies context.env['indent_width']
+        inner_indent = indent * (context.env["indent_width"] // len(indent) - 1)
+        for child in node.children:
+            content = child.render(context)
+            # inner_items = content.split("\n")
+            rendered += f"{inner_indent}{bullet} {content}\n"
+            # # Process nested code blocks
+            # if len(inner_items) > 1:
+            #     nested_conent = textwrap.indent(
+            #         "\n".join(inner_items[1:]),
+            #         inner_indent + indent,
+            #     )
+            #     rendered += f"\n{nested_conent}\n"
+    return rendered
 
 
 # A mapping from syntax tree node type to a function that renders it.
 # This can be used to overwrite renderer functions of existing syntax
 # or add support for new syntax.
 RENDERERS: Mapping[str, Render] = {
-    "bullet_list": partial(_render_list, message="bullet_list"),
-    "ordered_list": partial(_render_list, message="ordered_list"),
-    "list_item": partial(_render_list, message="list_item"),
+    "bullet_list": partial(_render_list, bullet="-"),
+    "ordered_list": partial(_render_list, bullet="1."),
 }
--- tests/fixtures.md
+++ tests/fixtures.md
@@ -8,6 +8,40 @@ Dashed list
     - item 2
 .
 
+Asterisk list
+.
+* item 1
+    * item 2
+.
+- item 1
+    - item 2
+.
+
+Numbered list
+.
+1. item 1
+    1. item 2
+    2. item 2
+        1. item 3
+        2. item 3
+.
+1. item 1
+    1. item 2
+    1. item 2
+        1. item 3
+        1. item 3
+.
+
+Combination list
+.
+- item 1
+    * item 2
+        1. item 3
+.
+- item 1
+    - item 2
+        1. item 3
+.
 
 Corrected Indentation from 3x
 .
@@ -35,15 +69,28 @@ Corrected Indentation from 5x
             - item 4
 .
 
-List with code block
+FIXME: List with (what should be converted to a) code block
 .
 - item 1
     
     code block
 .
 - item 1
-    
+
+code block
+.
+
+FIXME: List with explicit code block (that should keep indentation)
+.
+- item 1
+        
     ```
     code block
     ```
+.
+- item 1
+
+```
+code block
+```
 .
