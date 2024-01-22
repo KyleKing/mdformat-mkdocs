3cc34037255dd06823bcff27d562c2883f82bddb
Kyle King <KyleKing@users.noreply.github.com>
2024-01-22 08:07:51 -0500

    test: switch to syrupy

--- tests/format/test_parsed_result.py
+++ tests/format/test_parsed_result.py
@@ -1,12 +1,9 @@
-import json
 from pathlib import Path
 
 import pytest
 from markdown_it.utils import read_fixture_file
 
 from mdformat_mkdocs._indent import process_text
-
-from ..helpers import print_text
 
 FIXTURE_PATH = Path(__file__).parent / "fixtures/parsed_result.md"
 fixtures = read_fixture_file(FIXTURE_PATH)
@@ -17,7 +14,7 @@ fixtures = read_fixture_file(FIXTURE_PATH)
     fixtures,
     ids=[f[1] for f in fixtures],
 )
-def test_parsed_result(line, title, text, expected):
+def test_parsed_result(line, title, text, expected, snapshot):
     # TODO: Read these settings from the 'title'
     number_mode = False
     use_sem_break = False
@@ -28,8 +25,4 @@ def test_parsed_result(line, title, text, expected):
         number_mode=number_mode,
         use_sem_break=use_sem_break,
     )
-
-    parsed_result = output._asdict()
-    pretty_parsed_result = json.dumps(parsed_result, indent=4)
-    print_text(pretty_parsed_result, expected)
-    assert pretty_parsed_result == expected.strip()
+    assert output == snapshot
