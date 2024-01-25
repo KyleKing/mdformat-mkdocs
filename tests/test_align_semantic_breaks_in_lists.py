a2771a75eb1facbff91414bf19781805057b041c
Kyle King <KyleKing@users.noreply.github.com>
2024-01-24 22:52:25 -0500

    Revert "hack: temporarily skip semantic break testing"
    This reverts commit a4fbf9bf420b68f9661a1cbfa786f2890e7a68a2.

--- tests/test_align_semantic_breaks_in_lists.py
+++ tests/test_align_semantic_breaks_in_lists.py
@@ -1,25 +1,25 @@
-# from pathlib import Path
+from pathlib import Path
 
-# import mdformat
-# import pytest
-# from markdown_it.utils import read_fixture_file
+import mdformat
+import pytest
+from markdown_it.utils import read_fixture_file
 
-# from .helpers import print_text
+from .helpers import print_text
 
-# FIXTURE_PATH = Path(__file__).parent / "fixtures-semantic-indent.md"
-# fixtures = read_fixture_file(FIXTURE_PATH)
+FIXTURE_PATH = Path(__file__).parent / "fixtures-semantic-indent.md"
+fixtures = read_fixture_file(FIXTURE_PATH)
 
 
-# @pytest.mark.parametrize(
-#     ("line", "title", "text", "expected"),
-#     fixtures,
-#     ids=[f[1] for f in fixtures],
-# )
-# def test_align_semantic_breaks_in_lists(line, title, text, expected):
-#     output = mdformat.text(
-#         text,
-#         options={"align_semantic_breaks_in_lists": True, "wrap": "keep"},
-#         extensions={"mkdocs"},
-#     )
-#     print_text(output, expected)
-#     assert output.rstrip() == expected.rstrip()
+@pytest.mark.parametrize(
+    ("line", "title", "text", "expected"),
+    fixtures,
+    ids=[f[1] for f in fixtures],
+)
+def test_align_semantic_breaks_in_lists(line, title, text, expected):
+    output = mdformat.text(
+        text,
+        options={"align_semantic_breaks_in_lists": True, "wrap": "keep"},
+        extensions={"mkdocs"},
+    )
+    print_text(output, expected)
+    assert output.rstrip() == expected.rstrip()
