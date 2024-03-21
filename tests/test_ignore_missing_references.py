0865fda5b34481a7e4e8498dc3ca29098f0bf9f1
Kyle King <KyleKing@users.noreply.github.com>
2024-03-20 21:47:20 -0400

    test(#19): verify logic from GitHub issue

---
+++ tests/test_ignore_missing_references.py
@@ -0,0 +1,25 @@
+import mdformat
+import pytest
+
+from .helpers import print_text
+
+TICKET_019 = """[package.module.object][]
+[Object][package.module.object]
+"""
+
+
+@pytest.mark.parametrize(
+    ("text", "expected"),
+    [
+        (TICKET_019, TICKET_019),
+    ],
+    ids=["TICKET_019"],
+)
+def test_align_semantic_breaks_in_lists(text, expected):
+    output = mdformat.text(
+        text,
+        options={"ignore_missing_reference": True},
+        extensions={"mkdocs"},
+    )
+    print_text(output, expected)
+    assert output == expected
