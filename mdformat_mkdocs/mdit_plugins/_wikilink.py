ec77c87356e2c8e78300ebbf83f453bdb1f2c1ce
Kyle King <KyleKing@users.noreply.github.com>
2024-03-20 21:35:49 -0400

    refactor: improve wikilink logic

--- mdformat_mkdocs/mdit_plugins/_wikilink.py
+++ mdformat_mkdocs/mdit_plugins/_wikilink.py
@@ -1,4 +1,4 @@
-"""Copied from `mdformat-wikilink`.
+"""Adapted from `mdformat-wikilink`.
 
 https://github.com/tmr232/mdformat-wikilink/blob/d095be227a3cefc18d6fd823d9eb6fdfeaa5c895/src/mdformat_wikilink/mdit_wikilink_plugin.py
 
@@ -9,10 +9,13 @@ import re
 from markdown_it import MarkdownIt
 from markdown_it.rules_inline import StateInline
 
-LINK_PATTERN = re.compile(r"\[\[([^[|\]\n])+(\|[^]\n]+)?]]")
+LINK_PATTERN = re.compile(r"\[\[([^[|\]\n]+)(\|[^]\n]+)?\]\]")
 
 
 def _wikilink_inline(state: StateInline, silent: bool) -> bool:
+    if state.src[state.pos] != "[":  # Exit quickly
+        return False
+
     match = LINK_PATTERN.match(state.src[state.pos :])
     if not match:
         return False
