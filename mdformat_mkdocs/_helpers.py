d92018362af8f69b30e507aedd552e2823cd0fd5
Kyle King <KyleKing@users.noreply.github.com>
2024-08-22 08:28:42 -0400

    ci: apply ruff auto-fix

--- mdformat_mkdocs/_helpers.py
+++ mdformat_mkdocs/_helpers.py
@@ -40,5 +40,5 @@ def separate_indent(line: str) -> tuple[str, str]:
     """
     re_indent = re.compile(r"(?P<indent>\s*)(?P<content>[^\s]?.*)")
     match = re_indent.match(line)
-    assert match is not None  # for pyright # noqa: S101
+    assert match is not None  # for pyright
     return (match["indent"], match["content"])
