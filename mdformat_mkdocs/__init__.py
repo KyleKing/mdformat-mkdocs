8dff21774befe2b8272d340a5f4f3ee17275a4da
Kyle King <KyleKing@users.noreply.github.com>
2024-12-15 10:20:05 -0500

    build: copier-auto-update

--- mdformat_mkdocs/__init__.py
+++ mdformat_mkdocs/__init__.py
@@ -6,10 +6,4 @@ __version__ = "3.1.1"
 #   https://github.com/executablebooks/mdformat/blob/5d9b573ce33bae219087984dd148894c774f41d4/src/mdformat/plugins.py
 from .plugin import POSTPROCESSORS, RENDERERS, add_cli_options, update_mdit
 
-__all__ = (
-    "POSTPROCESSORS",
-    "RENDERERS",
-    "__version__",
-    "add_cli_options",
-    "update_mdit",
-)
+__all__ = ("POSTPROCESSORS", "RENDERERS", "add_cli_options", "update_mdit")
