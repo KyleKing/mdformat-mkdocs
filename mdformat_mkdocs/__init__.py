919fb55fd8c848b328582ddad2ae868bcf6e1a00
Kyle King <KyleKing@users.noreply.github.com>
2024-12-16 20:47:15 -0500

    release: v4.1.0

--- mdformat_mkdocs/__init__.py
+++ mdformat_mkdocs/__init__.py
@@ -1,6 +1,6 @@
 """An mdformat plugin for `mkdocs`."""
 
-__version__ = "4.0.0"
+__version__ = "4.1.0"
 
 # FYI see source code for available interfaces:
 #   https://github.com/executablebooks/mdformat/blob/5d9b573ce33bae219087984dd148894c774f41d4/src/mdformat/plugins.py
