f38e16ff8255942d8b9c8a536f1b01fdb3459f90
Kyle King <KyleKing@users.noreply.github.com>
2024-12-16 20:25:40 -0500

    refactor: extract while loop

--- mdformat_mkdocs/mdit_plugins/_pymd_snippet.py
+++ mdformat_mkdocs/mdit_plugins/_pymd_snippet.py
@@ -22,6 +22,7 @@ from mdformat_mkdocs._synced.admon_factories import new_token
 
 _SNIPPET_MARKER = "--8<--"
 _ABBREVIATION_PATTERN = re.compile(rf"^{_SNIPPET_MARKER}(?P<label>.*)")
+
 PYMD_SNIPPET_PREFIX = "pymd_snippet"
 
 
@@ -30,6 +31,35 @@ def _content(state: StateBlock, start_line: int) -> str:
     start = state.bMarks[start_line] + state.tShift[start_line]
     maximum = state.eMarks[start_line]
     return state.src[start:maximum]
+
+
+def _parse(
+    state: StateBlock,
+    match: re.Match[str],
+    start_line: int,
+    end_line: int,
+) -> tuple[int, str]:
+    """Return the max line and matched content."""
+    max_line = start_line + 1
+    inline = f"{_SNIPPET_MARKER}{match['label']}"
+
+    if not match["label"]:
+        max_search = 20  # Upper limit of lines to search for multi-line snippet
+        current_line = start_line + 1
+        inner: list[str] = []
+        while (current_line - start_line) < max_search:
+            if max_line == end_line:
+                break  # no 'label'
+            line = _content(state, current_line)
+            if _ABBREVIATION_PATTERN.match(line):
+                max_line = current_line + 1
+                inline = "\n".join([_SNIPPET_MARKER, *inner, _SNIPPET_MARKER])
+                break
+            if line:
+                inner.append(line)
+            current_line += 1
+
+    return max_line, inline
 
 
 def _pymd_snippet(
@@ -48,24 +78,7 @@ def _pymd_snippet(
     if silent:
         return True
 
-    max_line = start_line + 1
-    inline = f"{_SNIPPET_MARKER}{match['label']}"
-
-    if not match["label"]:
-        max_search = 20  # Upper limit of lines to search for multi-line snippet
-        current_line = start_line + 1
-        inner = []
-        while (current_line - start_line) < max_search:
-            if max_line == end_line:
-                break  # no 'label'
-            line = _content(state, current_line)
-            if match := _ABBREVIATION_PATTERN.match(line):
-                max_line = current_line + 1
-                inline = "\n".join([_SNIPPET_MARKER, *inner, _SNIPPET_MARKER])
-                break
-            if line:
-                inner.append(line)
-            current_line += 1
+    max_line, inline = _parse(state, match, start_line, end_line)
 
     with new_token(state, PYMD_SNIPPET_PREFIX, "p"):
         tkn_inline = state.push("inline", "", 0)
