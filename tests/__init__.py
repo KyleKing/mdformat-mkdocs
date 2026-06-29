3aa039508cae8bf4f590e6e7c54f22559a0c2e3c
Kyle King <KyleKing@users.noreply.github.com>
2026-06-29 14:47:42 -0600

    build: copier-auto-update

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,15 @@
-"""Tests for mdformat-mkdocs."""
+# ruff: noqa: RUF067
+"""Ignore beartype warnings from tests."""
+
+from contextlib import suppress
+
+with suppress(ImportError):  # Only suppress beartype warnings when installed
+    from warnings import filterwarnings
+
+    from beartype.roar import (
+        BeartypeClawDecorWarning,  # Too many False Positives using NamedTuples
+        BeartypeDecorHintPep585DeprecationWarning,
+    )
+
+    filterwarnings("ignore", category=BeartypeClawDecorWarning)
+    filterwarnings("ignore", category=BeartypeDecorHintPep585DeprecationWarning)
